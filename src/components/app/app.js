e9ed9ce76556ef8a923caf5575924b6703264bb2
ahmadkhaleel96 <ahmadkhaleel96@yahoo.com>
2020-03-21 20:12:47 +0200

    still working

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,14 +2,16 @@
 import React from 'react';
 // import your path to your class;
 import Header from '../Header/Header.js';
-// import Footer from '../Footer/Footer.js';
-// import CarRentForm from '../carRentForm/car-rent-form.js';
+import Footer from '../Footer/Footer.js';
+import CarRentForm from '../carRentForm/car-rent-form.js';
 import './app.scss';
 
 export default function App (){
   return (
     <>
       <Header />
+      <CarRentForm />
+      <Footer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/app/app.js';
+import App from './components/app/app.js';
 
 function Main() {
   return (
     <>
-      <h1> hi </h1>
+      <App />
     </>
   );
 }
